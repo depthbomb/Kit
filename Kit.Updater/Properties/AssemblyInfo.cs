35bdae43bdc72ed17a8facf864942cd343932bd2
depthbomb <depthbomb@users.noreply.github.com>
2026-05-23 11:03:17 -0500

    chore: update assembly metadata

--- Kit.Updater/Properties/AssemblyInfo.cs
+++ Kit.Updater/Properties/AssemblyInfo.cs
@@ -1,13 +1,11 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Kit.Updater")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("Kit updater binary")]
+[assembly: AssemblyDescription("Kit updater binary")]
 [assembly: AssemblyCompany("Caprine Logic")]
 [assembly: AssemblyProduct("Kit.Updater")]
 [assembly: AssemblyCopyright("Copyright © Caprine Logic 2026")]
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
